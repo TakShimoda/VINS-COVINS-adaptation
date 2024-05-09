f45f03b690fab1f6b1bcf126bd6780043c6fa4a1
TakShimoda <31490076+TakShimoda@users.noreply.github.com>
2024-05-09 11:22:15 -0400

    Update run_vins_covins_euroc.sh
    Added line to export ROS_MASTER_URI to properly use the same ros master in docker.
    Changed name of catkin_ws to catkin_vins.
    Changed ${ROS_DISTRO} (which returns noetic) to melodic.

--- docker/run_vins_covins_euroc.sh
+++ docker/run_vins_covins_euroc.sh
@@ -28,19 +28,22 @@ function absPath()
 }
 
 VINS_FUSION_DIR=$(absPath "..")
-CONFIG_IN_DOCKER="/root/catkin_ws/src/VINS-Fusion/config/euroc/euroc_mono_imu_config.yaml"
+CONFIG_IN_DOCKER="/root/catkin_vins/src/VINS-Fusion/config/euroc/euroc_mono_imu_config.yaml"
+HOST_IP=$(hostname -I | awk '{print $1}')
+echo "Host IP: ${HOST_IP}";
 
 docker run \
 -it \
 --rm \
 --net=host \
--v ${VINS_FUSION_DIR}:/root/catkin_ws/src/VINS-Fusion/ \
+-v ${VINS_FUSION_DIR}:/root/catkin_vins/src/VINS-Fusion/ \
 ros:vins-fusion \
 /bin/bash -c \
-"cd /root/catkin_ws/; \
+"export ROS_MASTER_URI=http://${HOST_IP}:11311; \
+cd /root/catkin_vins/; \
 catkin config \
         --env-cache \
-        --extend /opt/ros/$ROS_DISTRO \
+        --extend /opt/ros/melodic \
     --cmake-args \
         -DCMAKE_BUILD_TYPE=Release; \
     catkin build; \
